0d9b5e7fc3e7b21424159ef19ead2dff70b80312
reoh <1750913582@qq.com>
2020-03-04 23:21:17 +0800

    Japanese dictionary check

--- dictionary/selfCheck.rb
+++ dictionary/selfCheck.rb
@@ -1,7 +1,20 @@
 $location = 1
 
+def format(line)
+  if line =~ /「.*」「.*」「.*」/
+    $location += 1
+  elsif line =~/^#/
+    $location += 1
+  else
+    puts line.inspect
+    p line.chomp.empty?
+    puts $location
+    $location += 1
+  end
+end
+
 def if_empty(line)
-  puts $location if line.empty?
+  puts $location if line.chomp.empty?
 end
 
 def part_of_speech(line)
@@ -36,17 +49,6 @@ def part_of_speech(line)
         p $location
       end
     end
-  end
-end
-
-def format(line)
-  if line =~ /「.*」「.*」「.*」/
-    $location += 1
-  elsif line =~/^#/
-    $location += 1
-  else
-    puts $location
-    $location += 1
   end
 end
 
